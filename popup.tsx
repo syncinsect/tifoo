224a2ec7c0750f4fdf594ea6783451f7da2ca77c
actopas <hi@actopas.me>
2024-10-16 05:54:04 +0800

    feat: update popup view

--- popup.tsx
+++ popup.tsx
@@ -1,24 +1,35 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+import "./style.css"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [isActive, setIsActive] = useState(false)
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0]?.id) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          action: "toggleScanner",
+          isActive
+        })
+      }
+    })
+  }, [isActive])
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="popup">
+      <h1>Tailware</h1>
+      <div className="toggle">
+        <span>Now is {isActive ? "active" : "inactive"}</span>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={isActive}
+            onChange={() => setIsActive(!isActive)}
+          />
+          <span className="slider round"></span>
+        </label>
+      </div>
     </div>
   )
 }
