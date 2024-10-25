0db688c7c8df69119ea50ba24866aa41e7057b58
actopas <hi@actopas.me>
2024-10-25 22:44:59 +0800

    fix: default output of inject styles

--- src/utils/tailwindUtils.ts
+++ src/utils/tailwindUtils.ts
@@ -112,7 +112,7 @@ function injectTailwindClass(className: string): void {
           styleContent = `.group:${groupPrefix} .${escapedClassName} { ${classData.p} }`
           break
         default:
-          styleContent = `body .${escapedClassName}:not([class*="sm:"]):not([class*="md:"]):not([class*="lg:"]):not([class*="xl:"]):not([class*="2xl:"]) { ${classData.p} }`
+          styleContent = `.${escapedClassName} { ${classData.p} }`
       }
 
       injectStyle(styleContent)
@@ -121,7 +121,7 @@ function injectTailwindClass(className: string): void {
     classData = tailwindClasses.find(({ c }) => c === className)
     if (classData) {
       const escapedClassName = className.replace(/:/g, "\\:")
-      const styleContent = `body .${escapedClassName}:not([class*="sm:"]):not([class*="md:"]):not([class*="lg:"]):not([class*="xl:"]):not([class*="2xl:"]) { ${classData.p} }`
+      const styleContent = `.${escapedClassName} { ${classData.p} }`
       injectStyle(styleContent)
     }
   }
