38245361deee88bf260883f73cbf57c8ba110375
actopas <hi@actopas.me>
2024-10-25 22:13:54 +0800

    chore: remove redundant style file

--- popup.tsx
+++ popup.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 
-import "./style.css"
+import "@/styles/globals.css"
 
 function IndexPopup() {
   const [isActive, setIsActive] = useState(false)
